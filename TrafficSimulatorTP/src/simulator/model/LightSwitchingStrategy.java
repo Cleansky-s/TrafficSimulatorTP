ae98e773413337cf7fd025687064914e1ad67d4c
Cleansky-s <69647070+Cleansky-s@users.noreply.github.com>
2022-02-19 16:11:24 +0100

    Delete TrafficSimulatorTP directory

--- TrafficSimulatorTP/src/simulator/model/LightSwitchingStrategy.java
+++
@@ -1,8 +0,0 @@
-package simulator.model;
-
-import java.util.List;
-
-public interface LightSwitchingStrategy {
-	int chooseNextGreen(List<Road> roads, List<List<Vehicle>> qs, int currGreen, int lastSwitchingTime, int currTime);
-
-}
--- TrafficSimulatorTP/src/simulator/model/VehicleStatus.java
+++
@@ -1,5 +0,0 @@
-package simulator.model;
-
-public enum VehicleStatus {
-	PENDING, TRAVELING, WAITING, ARRIVED;
-}
